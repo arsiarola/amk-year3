c298466080440b0808d719fcc89ee849d1ff7793
arsiarola <1andre234@gmail.com>
2021-03-18 17:58:40 +0200

    unix week1 3.c done

--- c/unix/week1/3.c
+++ c/unix/week1/3.c
@@ -1,23 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 extern char *optarg;
 extern int optind, opterr,optopt;
-int main(int argc, char *argv[]) {
-	int flags=0, opt, nsecs=0, tfnd=0;
-	while ((opt = getopt(argc, argv, "nt:")) != -1) {
-		switch (opt) {
-			case 'n': flags = 1; break;
-			case 't': tfnd = 1; nsecs = atoi(optarg); break;
-			default: /* '?' */
-				  fprintf(stderr, "Usage: %s [-t nsecs] [-n] name\n", argv[0]);
-				  exit(EXIT_FAILURE);
-		}
-	}
-	printf("flags=%d; tfnd=%d; optind=%d\n", flags, tfnd, optind);
-	if (optind >= argc) {
-		fprintf(stderr, "Expected argument after options\n");
-		exit(EXIT_FAILURE);
-	}
-	printf("name argument = %s\n", argv[optind]);
-	/* Tähän sovelluksen varsinainen koodi */
-	exit(EXIT_SUCCESS);
+int
+main(int argc, char *argv[]) {
+        int opt;
+        // Reorder arguments, executable name will be first
+        // Actual argument will be last, everything else in between
+        while ((opt = getopt(argc, argv, "a:s:m:d:")) != -1) {
+                switch (opt) {
+                case '?':
+                        fprintf(stderr, "Expected argument after options\n");
+                        exit(EXIT_FAILURE);
+                        break;
+                }
+        }
+
+        optind = 1; // reset index back to 1 so we can loop again
+        int result = atoi(argv[argc - 1]);
+        while ((opt = getopt(argc, argv, "a:s:m:d:")) != -1) {
+                int value = atoi(optarg);
+                switch (opt) {
+                case 'a':
+                        result += value;
+                        break;
+                case 's':
+                        result -= value;
+                        break;
+                case 'm':
+                        result *= value;
+                        break;
+                case 'd':
+                        if (value == 0) {
+                                printf("Not allowed to divide by zero\n");
+                                exit(EXIT_FAILURE);
+                        }
+                        result /= value;
+                        break;
+                default: /* '?' */
+                        fprintf(stderr,
+                                "Usage: %s result [-a n] [-s n] [-m n] [-d n]\n",
+                                argv[0]);
+                        exit(EXIT_FAILURE);
+                }
+        }
+        printf("%d\n", result);
+        exit(EXIT_SUCCESS);
 }
