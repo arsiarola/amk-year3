3d7382293537cc4a23b90764dafb452d05c5c4bf
arsiarola <1andre234@gmail.com>
2021-04-20 20:29:49 +0300

    unix 8 started

--- robot/5-stepper/test.py
+++ robot/5-stepper/test.py
@@ -2,14 +2,32 @@ import serial
 import time
 
 
-port = serial.Serial('/dev/ttyS3', 115200, timeout = 1)
+capture = serial.Serial('/dev/ttyS3', 115200, timeout = 1)
+#init
+# while True:
+#     c = capture.read(1).decode('utf-8')
+#     print("c = ", c)
+#     if c == 'Q':
+#         break
+limits = "svut"
+
+capture.write(bytes('q', "utf-8"))
+capture.write(bytes(chr(100), "utf-8"))
+capture.write(bytes(chr(150), "utf-8"))
+capture.write(bytes(chr(1), "utf-8"))
+capture.write(bytes('O', "utf-8"))
+ok = capture.read(4)
+capture.write(bytes('J', "utf-8"))
+
+# print(ok)
+# print(all(c in str(ok) for c in limits))
+
 try:
     while True:
-        print(port.read())
+        print(capture.read())
 except KeyboardInterrupt:
     print("Keyboard Interrupt")
 
 # class MorseDecoderLibrary(object):
 #     def __init__(self, port):
 #         self.port = serial.Serial(port, 115200, timeout = 1)
-
