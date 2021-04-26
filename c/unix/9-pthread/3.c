70cb1d2b762ed31f85a1a1153e79eee1ace76d6d
arsiarola <1andre234@gmail.com>
2021-04-26 20:56:25 +0300

    unix 9 done

--- c/unix/9-pthread/3.c
+++ c/unix/9-pthread/3.c
@@ -5,51 +5,59 @@
 #include <errno.h>
 #include <pthread.h>
 
-#define SIZE 10
+#define SIZE 5
 int array[SIZE] = { 0 };
+
 pthread_mutex_t raffling    = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t calculating = PTHREAD_MUTEX_INITIALIZER;
 
 void err_exit(const char *errmsg);
 
 void *raffle(void *arg) {
-        for(int j = 0; j < 10; ++j) for(int i = 0; i < SIZE; ++i) {
+        for(int j = 0; j < 10; ++j) {
                 pthread_mutex_lock(&raffling);
-                array[i] = rand() % 1000;
+                for(int i = 0; i < SIZE; ++i) {
+                        array[i] = rand() % 1000;
+                }
                 pthread_mutex_unlock(&calculating);
+        }
+
+        // end mark for array
+        for(int i = 0; i < SIZE; ++i) {
+                array[i] = -1;
         }
         pthread_exit(NULL);
 }
 
 void *calc_mean(void *arg) {
-        pthread_mutex_lock(&calculating);
-        int mean;
         int total;
         while(1) {
                 pthread_mutex_lock(&calculating);
-                mean = 0;
                 total = 0;
                 for(int i = 0; i < SIZE; ++i) {
+                        // numbers were supposed to be 0-999, so even if
+                        // one of them is negative, end the thread
                         if (array[i] == -1)
                                 goto break_loop;
+
                         total += array[i];
                 }
-                printf("mean = %d\n", mean);
-                pthread_mutex_lock(&raffling);
+                printf("mean = %d\n", total / SIZE);
+                pthread_mutex_unlock(&raffling);
         }
 break_loop:
+
         pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-        time_t t;
-        srand((unsigned) time(&t));
-
         pthread_t tid1, tid2;
-        pthread_mutex_lock(&calculating);
+        pthread_mutex_lock(&calculating); // calculate thread has to wait for numbers first
 
-        pthread_create(&tid1, NULL, raffle, NULL);
-        pthread_create(&tid2, NULL, mean,   NULL);
+        srand(time(NULL));
+
+        pthread_create(&tid1, NULL, raffle,    NULL);
+        pthread_create(&tid2, NULL, calc_mean, NULL);
 
         pthread_join(tid1, NULL);
         pthread_join(tid2, NULL);
